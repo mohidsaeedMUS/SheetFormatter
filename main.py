f0563fc727053c6140ba36a48853ff62a9f336d3
Mohid Saeed <mohid.saeed@musowls.org>
2022-06-09 15:07:04 -0500

    Added sample code fille and TODO

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ import openpyxl
 wrkbk = openpyxl.load_workbook("Test.xlsx")  
 sh = wrkbk.active
 
-  
-# iterate through excel and display data
-# for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
-#    for cell in col:
-#        print(cell.value, end=" ")
-#    print()
+# TODO: Prompt user for email status columns
+# TODO: Translate user-input letters into numbers
 
 for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
     for cell in col:
---
+++ sample_code.py
@@ -0,0 +1,6 @@
+# Iterates thru col and prints out cell
+# iterate through excel and display data
+# for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
+#    for cell in col:
+#        print(cell.value, end=" ")
+#    print()
