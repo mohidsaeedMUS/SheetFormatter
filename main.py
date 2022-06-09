b2946c25517dd1ff0b3e79dbaf74f4603d8ed6a5
Mohid Saeed <mohid.saeed@musowls.org>
2022-06-09 14:43:11 -0500

    Using openpyxl to loop thru col and access email status

---
+++ main.py
@@ -0,0 +1,20 @@
+import openpyxl
+  
+# load excel with its path
+wrkbk = openpyxl.load_workbook("Test.xlsx")  
+sh = wrkbk.active
+sh_new = wrkbk.create_sheet(title = "Linkedin Only")
+  
+# iterate through excel and display data
+# for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
+#    for cell in col:
+#        print(cell.value, end=" ")
+#    print()
+
+for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
+    for cell in col:
+        if cell.value.casefold() == "unavailable":
+            print(True)
+            break
+    print()
+wrkbk.save("Test.xlsx")
