d2ecb9461ea8ea9930f1dc3017d8028af465d31f
amarka8 <amar.kanakamedala@gmail.com>
2022-06-11 15:49:01 -0500

    Finished email section
    it needs some touching up

--- email_prelim.py
+++ email_prelim.py
@@ -1,5 +1,4 @@
 import openpyxl
-#gets first active sheet and creates a workbook
 wb = openpyxl.load_workbook("Test_in_vscode.xlsx")
 ws=wb.active
 # column_string=input('Enter Column Letter with Email:')
@@ -7,7 +6,7 @@ ws=wb.active
 # column=openpyxl.utils.cell.column_index_from_string(column_string)
 # count=0
 # start=1
-#grab a row and input it into other sheet then delete it
+#grab a row and input it into other sheet then delete it 
 # for row in ws.iter_rows(min_col=column,max_col=column,min_row=2,max_row=ws.max_row):
     # for cell in row:
     #     start+=1
@@ -17,19 +16,31 @@ ws=wb.active
     #             ws1=wb.create_sheet('Linkedin Only')
     #             ws1.append(["First Name","Last Name","Company Name","Email","Email Status","First Phone","Employees","Industry","Person", "Linkedin","Company City","Company State","Company Count"])
     #             ws.delete_rows(start,1)
+mr=ws.max_row
+mc=ws.max_column
 column_string=input('Enter Column Letter with Email:')
 #need to be able to move on to next if left blank
-column=openpyxl.utils.cell.column_index_from_string(column_string)
+column=openpyxl.utils.cell.column_index_from_string(column_string) 
 count=0
-#grab a row if meets condition and input it into other sheet then delete it from current sheet
+row=1
+# grab a row if meets condition and input it into other sheet then delete it from current sheet
 for col in ws.iter_cols(min_row = 2, max_row = ws.max_row, min_col = column , max_col = column):
     for cell in col:
+        row+=1
         if cell.internal_value == "Unavailable":
             count+=1
             if count==1:
-                # for row in ws.iter_rows(min_col=column,max_col=column,min_row=,max_row=ws.max_row):
                 sh_new = wb.create_sheet(title = "Linkedin Only")
                 sh_new.append(["First Name","Last Name","Company Name","Email","Email Status","First Phone","Employees","Industry","Person", "Linkedin","Company City","Company State","Company Count"])
+                for i in range (row, row+1):
+                    for j in range (1, mc + 1):
+                        c = ws.cell(row = i, column = j)
+                        sh_new.cell(row = i, column = j).value = c.value
+            else:
+                for i in range (row, row+1):
+                    for j in range (1, mc + 1):
+                        c = ws.cell(row = i, column = j)
+                        sh_new.cell(row = i, column = j).value = c.value
                 #grab row data
                 #add to new sheet
                 #delete from ws
---
+++ email_scratch.py
@@ -0,0 +1,34 @@
+import openpyxl
+wb = openpyxl.load_workbook("Test_in_vscode.xlsx")
+ws=wb.active
+sh_new = wb.create_sheet(title = "Linkedin Only")
+sh_new.append(["First Name","Last Name","Company Name","Email","Email Status","First Phone","Employees","Industry","Person", "Linkedin","Company City","Company State","Company Count"])
+mr=ws.max_row
+mc=ws.max_column
+rownum=1
+column_string=input('Enter Column Letter with Email:')
+#need to be able to move on to next if left blank
+# column=openpyxl.utils.cell.column_index_from_string(column_string) 
+for i in range (1, mr +1):
+    for j in range (1, mc + 1):
+        c = ws.cell(row = i, column = j)
+        sh_new.cell(row = i, column = j).value = c.value
+# for col in sh_new.iter_cols(min_row = 2, max_row = ws.max_row, min_col = column , max_col = column):
+#     for cell in col:
+#         rownum+=1
+#         if cell.internal_value == "Verified":
+# for row in sh_new.rows:
+#     for cell in row:
+#         if cell.value =="Verified":
+#             sh_new.delete_rows(cell.row, 1)
+for cell in sh_new[column_string][1:]:
+    if cell.value == "Verified":
+        sh_new.delete_rows(cell.row)
+for cell in ws[column_string][1:]:
+    if cell.value == "Unavailable":
+        ws.delete_rows(cell.row)
+wb.save("Test_in_vscode.xlsx")
+#my worry- if doesnt have words verified or unavailable it wont work / if do not capitalize column_string it wont work
+
+    
+
