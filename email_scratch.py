6d6f3e1313c77448429051241f5027bf3ee00f73
amarka8 <amar.kanakamedala@gmail.com>
2022-06-13 15:11:18 -0500

    Updates to Email

--- email_scratch.py
+++ email_scratch.py
@@ -1,18 +1,29 @@
 import openpyxl
 wb = openpyxl.load_workbook("Test.xlsx")
 ws=wb.active
-sh_new = wb.create_sheet(title = "Linkedin Only")
-sh_new.append(["First Name","Last Name","Company Name","Email","Email Status","First Phone","Employees","Industry","Person", "Linkedin","Company City","Company State","Company Count"])
 mr=ws.max_row
 mc=ws.max_column
-rownum=1
-column_string=input('Enter Column Letter with Email:')
+count=0
+column_string=input("Enter Column Letter with Email (A or B or C or leave blank to skip editing):").upper()
+if len(column_string)>0:
+    for cell in ws[column_string][1:]:
+        if cell.value is None:
+            count+=1
+            if count==1:
+                ws_1=wb.create_sheet('Linkedin Only')
+                for i in range (1, mr +1):
+                    for j in range (1, mc + 1):
+                        c = ws.cell(row = i, column = j)
+                        ws_1.cell(row = i, column = j).value = c.value
+                for cell in ws_1[column_string][1:]:
+                    if cell.value is not None:
+                        ws_1.delete_rows(cell.row)
+                for cell in ws[column_string][1:]:
+                    if cell.value is None:
+                        ws.delete_rows(cell.row)
+    wb.save("Test.xlsx")
 #need to be able to move on to next if left blank
 # column=openpyxl.utils.cell.column_index_from_string(column_string) 
-for i in range (1, mr +1):
-    for j in range (1, mc + 1):
-        c = ws.cell(row = i, column = j)
-        sh_new.cell(row = i, column = j).value = c.value
 # for col in sh_new.iter_cols(min_row = 2, max_row = ws.max_row, min_col = column , max_col = column):
 #     for cell in col:
 #         rownum+=1
@@ -21,14 +32,9 @@ for i in range (1, mr +1):
 #     for cell in row:
 #         if cell.value =="Verified":
 #             sh_new.delete_rows(cell.row, 1)
-for cell in sh_new[column_string][1:]:
-    if cell.value == "Verified":
-        sh_new.delete_rows(cell.row)
-for cell in ws[column_string][1:]:
-    if cell.value == "Unavailable":
-        ws.delete_rows(cell.row)
-wb.save("Test.xlsx")
-#my worry- if doesnt have words verified or unavailable it wont work / if do not capitalize column_string it wont work/ if do not enter anything in column it has to skip
+else:
+    wb.save("Test.xlsx")
+#my worry- if email not empty but still want us to move it to linkedin only wont work 
 #do we arrange each of these capabilites in functions?
 
 
