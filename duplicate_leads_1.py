505cd71e3c5b2415abf00674ac96b8e039a842db
amarka8 <amar.kanakamedala@gmail.com>
2022-06-16 16:15:55 -0500

    duplicate leads

--- duplicate_leads_1.py
+++ duplicate_leads_1.py
@@ -1,12 +1,9 @@
-#input - enter column with name-do i do it by first name and last name?
-#do this before u do email or first
+#do this before u do email or first?
+#breaks down if not same headers for each sheet/user should be able to declare what duplicate is?
 import pandas as pd
-import openpyxl
-data = pd.read_excel('Test_in_vscode.xlsx',sheet_name=None)
+data = pd.read_excel('Test.xlsx',sheet_name=None)
 combined_df=pd.concat(data.values(), ignore_index=True)
 new_df=combined_df.drop_duplicates(subset=['First Name','Last Name'],keep='first')
 new_df.to_excel('Test_without_dups.xlsx', index = False, header=True)
-
-
     
 
--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import openpyxl
+# import openpyxl
   
 # load excel with its path
-wrkbk = openpyxl.load_workbook("Test.xlsx")  
-sh = wrkbk.active
+# wrkbk = openpyxl.load_workbook("Test.xlsx")  
+# sh = wrkbk.active
 
 # TODO: Prompt user for email status columns
 # TODO: Translate user-input letters into numbers
 
-for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
-    for cell in col:
-        if cell.value.casefold() == "unavailable":
-            sh_new = wrkbk.create_sheet(title = "Linkedin Only")
-            sh_new.append(["First Name","Last Name","Company Name","Email","Email Status","First Phone","Employees","Industry","Person", "Linkedin","Company City","Company State","Company Count"])
-            print(True)
-            break
-    print()
-wrkbk.save("Test.xlsx")
+# for col in sh.iter_cols(min_row = 2, max_row = 6, min_col = 5, max_col = 5):
+#     for cell in col:
+#         if cell.value.casefold() == "unavailable":
+#             sh_new = wrkbk.create_sheet(title = "Linkedin Only")
+#             sh_new.append(["First Name","Last Name","Company Name","Email","Email Status","First Phone","Employees","Industry","Person", "Linkedin","Company City","Company State","Company Count"])
+#             print(True)
+#             break
+#     print()
+# wrkbk.save("Test.xlsx")
 #tests
