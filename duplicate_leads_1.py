d50b7005fc598c019ea8dcc21066f9163d3ae59c
amarka8 <amar.kanakamedala@gmail.com>
2022-06-16 15:39:00 -0500

    duplicate leads

--- duplicate_leads_1.py
+++ duplicate_leads_1.py
@@ -1,10 +1,12 @@
 #input - enter column with name-do i do it by first name and last name?
 #do this before u do email or first
 import pandas as pd
-df = pd.read_excel('Test.xlsx', sheet_name=None)
-new_df={}
-for key,value in df.items():
-    new_value=value.drop_duplicates(subset=["First Name", "Last Name"], keep="first")
-    new_df[key]=new_value
-print(new_df)
+import openpyxl
+data = pd.read_excel('Test_in_vscode.xlsx',sheet_name=None)
+combined_df=pd.concat(data.values(), ignore_index=True)
+new_df=combined_df.drop_duplicates(subset=['First Name','Last Name'],keep='first')
+new_df.to_excel('Test_without_dups.xlsx', index = False, header=True)
+
+
+    
 
