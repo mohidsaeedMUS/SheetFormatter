84f996d40ae8e14dbddd61b9501b5c4907f64c7a
amarka8 <amar.kanakamedala@gmail.com>
2022-06-17 14:27:20 -0500

    more dups

--- duplicate_leads_1.py
+++ duplicate_leads_1.py
@@ -1,7 +1,7 @@
 #do this before u do email or first?
 #breaks down if not same headers for each sheet/user should be able to declare what duplicate is?
 import pandas as pd
-data = pd.read_excel('Test.xlsx',sheet_name=None)
+data = pd.read_excel('Test_in_vscode.xlsx',sheet_name=None)
 combined_df=pd.concat(data.values(), ignore_index=True)
 new_df=combined_df.drop_duplicates(subset=['First Name','Last Name'],keep='first')
 new_df.to_excel('Test_without_dups.xlsx', index = False, header=True)   
