e0e4e58eed4ceaddee3ca44f75201253a36e3fda
Mohid Saeed <mohid.saeed@musowls.org>
2022-06-28 19:39:59 -0500

    First_name_leads.py iterates through every sheet in the workbook

--- first_name_leads.py
+++ first_name_leads.py
@@ -1,16 +1,20 @@
 import openpyxl
-wb = openpyxl.load_workbook("another_test.xlsx")
-ws=wb.active
+from openpyxl import Workbook
 
-column_string=input('Enter Column Letter with First Name:')
-iter_column=openpyxl.utils.cell.column_index_from_string(column_string) 
+wb = openpyxl.load_workbook("Test_template.xlsx")
+#ws=wb.active
 
-i = 1
-while i <= ws.max_row:
-    if ws.cell(row=i, column=iter_column).value is None:
-        print(ws.cell(row=i,column=iter_column))
-        ws.delete_rows(i, 1)
-    else:
-        i+=1
+for sheet in wb.sheetnames:
+    print("Sheet: " + sheet)
+    ws = wb[sheet]
+    column_string=input('Enter Column Letter with First Name: ')
+    iter_column=openpyxl.utils.cell.column_index_from_string(column_string) 
+    i = 1
+    while i <= ws.max_row:
+        if ws.cell(row=i, column=iter_column).value is None:
+            #print(ws.cell(row=i, column=iter_column))
+            ws.delete_rows(i, 1)
+        else:
+            i +=1
 
-wb.save("another_test.xlsx")
+wb.save("Test_complete.xlsx")
