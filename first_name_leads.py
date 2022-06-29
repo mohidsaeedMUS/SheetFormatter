ea4a78bf0e0aa65b115576a4f449c970da8139f2
Mohid Saeed <mohid.saeed@musowls.org>
2022-06-28 19:48:02 -0500

    First_name_leads.py now checks for missing last names as well

--- first_name_leads.py
+++ first_name_leads.py
@@ -9,6 +9,8 @@ for sheet in wb.sheetnames:
     ws = wb[sheet]
     column_string=input('Enter Column Letter with First Name: ')
     iter_column=openpyxl.utils.cell.column_index_from_string(column_string) 
+    column_string_last=input('Enter Column Letter with Last Name: ')
+    iter_column_last=openpyxl.utils.cell.column_index_from_string(column_string_last)
     i = 1
     while i <= ws.max_row:
         if ws.cell(row=i, column=iter_column).value is None:
@@ -16,5 +18,13 @@ for sheet in wb.sheetnames:
             ws.delete_rows(i, 1)
         else:
             i +=1
+    j = 1
+    while j <= ws.max_row:
+        if ws.cell(row=j, column=iter_column_last).value is None:
+            #print(ws.cell(row=i, column=iter_column_last))
+            ws.delete_rows(j, 1)
+        else:
+            j +=1
+
 
 wb.save("Test_complete.xlsx")
